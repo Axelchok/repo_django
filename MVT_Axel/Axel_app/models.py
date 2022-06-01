64202fb860f3966b8360821d4eb8b4f49b74b945
Axelin <el.mago.chok@gmail.com>
2022-06-01 14:11:27 -0300

    Base SQL creada, pero sin los templates

--- MVT_Axel/Axel_app/models.py
+++ MVT_Axel/Axel_app/models.py
@@ -4,3 +4,13 @@ class Familiares(models.Model):
     documento = models.IntegerField()
     nombre = models.CharField(max_length=30)
     fecha = models.DateField()
+
+class Amigos(models.Model):
+    nombre = models.CharField(max_length=30)
+    deporte_favorito = models.CharField(max_length=30)
+    edad = models.IntegerField()
+
+class Tutores(models.Model):
+    nombre = models.CharField(max_length=30)
+    curso = models.CharField(max_length=30)
+    comision = models.CharField(max_length=30)
